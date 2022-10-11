caf45da3e87f4f2fbf5127e7ed30e4a0386e472a
andrew21-mch <nfonandrew73@gmail.com>
2022-10-11 05:17:24 +0100

    Day 7 arrays

---
+++ 30-Days-Java-With-Andy/src/Day7/Solution.java
@@ -0,0 +1,36 @@
+package Day7;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] arrTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrTemp[i]);
+            arr.add(arrItem);
+        }
+        
+        StringBuilder some = new StringBuilder();
+       for(int j = arr.size()-1; j >=0; j--){
+           some.append(arr.get(j)+ " ");
+       }
+       System.out.println(some);
+
+        bufferedReader.close();
+    }
+}
