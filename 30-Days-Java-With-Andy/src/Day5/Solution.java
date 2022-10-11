ade3f2e8da63d8e7c34d9d77c4b3bc9b8c973b89
andrew21-mch <nfonandrew73@gmail.com>
2022-10-11 04:09:52 +0100

    day 6 loops catchup

--- 30-Days-Java-With-Andy/src/Day5/Solution.java
+++ 30-Days-Java-With-Andy/src/Day5/Solution.java
@@ -1,10 +1,6 @@
 package Day5;
 
-import java.io.*;
 import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
  public static void main(String[] args) {
