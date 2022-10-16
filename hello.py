5419edeaab0631645ad72ff52223f55181ed853f
marcmiratechglobal <marcmiratechglobal@kali.local>
2022-10-16 05:01:53 -0400

    lauching of 30 days fun Reading an input and printing the content of the output

--- hello.py
+++ hello.py
@@ -1,2 +1,8 @@
-#laucnhing
+#laucnhing: Read a an input say a line or a stdin
+user_input_string = input()
+
+#print a lateral say 'hello_world'
 print('Hello World!')
+
+#get  the content of user input string and give an output
+print(user_input_string)
