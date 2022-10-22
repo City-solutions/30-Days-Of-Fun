48cdb01564bebfcd353338ddb417450b941d29d8
Jardon237 <ngwajude161@gmail.com>
2022-10-22 06:07:52 +0100

    day 5

---
+++ 30-Days-Python-with-Jude/day 5.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    for i in range(1,11):
+        multiple = n*i
+        print(f"{n} x {i} = {multiple}")
+        
+        
+
+       
+
